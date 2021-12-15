af059de8167637119fccf33c69160def8eac0665
anandubale <au26199@gmail.com>
2021-12-15 22:40:31 +0530

    Added emplyee Present or absent code

---
+++ EmployeePresentOrNot/EmployeePresentOrNot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EmpWageProbblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IsFullTime = 1;
+
+            //variables
+            Random random = new Random();
+
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IsFullTime)
+            {
+                Console.WriteLine("Employee is Present ");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent ");
+            }
+
+
+
+        }
+    }
+}
