85981cc6b20f501ff37ea933d52df5b3aa7f1b47
anandubale <au26199@gmail.com>
2021-12-15 23:20:20 +0530

    Added file for Employe part time full time wage

---
+++ EmployeePartFullTime/EmployeePartFullTime/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EmpWageHalfFull
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FullTime = 0;
+            int HalfTime = 1;
+            int EmpRatePerHour = 20;
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            //computation
+            int empCheck = random.Next(0, 3);
+            if (empCheck == FullTime)
+            {
+                empHrs = 8;
+                empWage = empHrs * EmpRatePerHour;
+                Console.WriteLine("Employee full Time Wage is " + empWage);
+
+            }
+            else if (empCheck == HalfTime)
+            {
+                empHrs = 4;
+                empWage = empHrs * EmpRatePerHour;
+                Console.WriteLine("Employee Half Time Wage is " + empWage);
+            }
+            else
+            {
+                empHrs = 0;
+                Console.WriteLine("Emplyee is Absent");
+
+            }
+
+
+        }
+    }
+}
